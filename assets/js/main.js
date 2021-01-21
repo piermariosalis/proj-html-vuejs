892295784b1b0684b807c928d1cb3d0c2c16501b
PierMario <piermario.salis@aol.dom>
2021-01-21 16:36:49 +0100

    Landrick - Proj-html-vue |  Update 9
    Footer - Istanza Vue con direttiva v-html per stampare i links nell'HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,26 @@ var app1 = new Vue({
     },
 
 })
+
+
+// App Vue per aggiungere links nel footer.
+
+// Nell'HTML grazie alla direttiva v-html, posso stampare i links e modifcarne lo stile.
+var app2 = new Vue({
+	el: '#vue_footer',
+	data: {
+		About: '<a href="#">About us</a>',
+		Services: '<a href="#">Services</a>',
+		Team: '<a href="#">Team</a>',
+		Pricing: '<a href="#">Pricing</a>',
+		Project: '<a href="#">Project</a>',
+		Careers: '<a href="#">Careers</a>',
+		Blog: '<a href="#">Blog</a>',
+		Login: '<a href="#">Login</a>',
+		Terms: '<a href="#">Terms of Services</a>',
+		Privacy: '<a href="#">Privacy Policy</a>',
+		Documentation: '<a href="#">Documentation</a>',
+		Changelog: '<a href="#">Changelog</a>',
+		Components: '<a href="#">Components</a>',
+	}
+})
